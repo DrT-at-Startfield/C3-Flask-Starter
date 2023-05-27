993b95c0ebb9cbfb6137a40d4c96429440742935
DrTrickey <19309941-DrTrickey@users.noreply.replit.com>
2023-05-27 19:43:58 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return 'Hello C3 participants! Brought to you by Startfield using Flask.'
+
+
+app.run(host='0.0.0.0', port=81)
